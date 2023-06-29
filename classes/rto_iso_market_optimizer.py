114d8cb2d2aea879fee13bd7f24e3453a3176901
Ellse1 <elias.singer@online.de>
2023-06-29 18:32:07 +0200

    first structure of optimizer, first variables added

--- classes/rto_iso_market_optimizer.py
+++ classes/rto_iso_market_optimizer.py
@@ -4,40 +4,61 @@ from classes.swing_contract_generator import SwingContractGenerator
 
 class MarketOptimizer:
 
-    #  Get all the Swing Contract Offers from the generators (do it directly here)
-    # This is a list of SwingContractGenerator objects
-    swing_contract_offers = []
-    swing_contract_offer1 = SwingContractGenerator()
-    swing_contract_offer1.offer_price_dollar = 10	
-    swing_contract_offer1.powermin_mw = 0
-    swing_contract_offer1.powermax_mw = 100
-
-    # add first swing contract to the list of swing contract offers
-    swing_contract_offers.append(swing_contract_offer1)
-
-    # put the first swing contract into the optimization problem
-
-
     # This example formulates and solves the model for the RTO-ISO market optimization problem:
     # In the very first attemp, a swing contract market should be designed for a Market M(T)  with T  having 24 k in K (24 hours for day T)
     # Price swing for the reserve offers should be included (price dependent on dispatch point) but the price function should be linear first. 
     # No price dependent reserve bids should be included first
 
+    # The Nodes that should be included in the Flow Model
+    nodes = ['A', 'B', 'C']
+    # In arcs a set of the arcs is defined ({('A', 'B'), ('A', 'C'), ('B', 'C')}). In capacity the capacity of the arcs is defined
+    arcs, capacity = gp.multidict({
+    ('A', 'B'):   100,
+    ('A', 'C'):  80,
+    ('B', 'C'):  120
+    })
+
+
+    # Each variable in one array (this is better for including it into gurobi)
+    offer_price_dollar = []
+    power_min_mw = []
+    power_max_mw = []
+    ramping_max_mw_up_per_k = []
+    ramping_max_mw_down_per_k = []
+    price_per_mw_h_dollar = []
+    is_cleared = []
+    # power_mw_in_step_k = [][]
+    number_of_swing_contract_offers = 0
+    number_of_time_steps_k_in_market = 24
+
+
     def optimize(self):
+        print("Start optimization")
 
         # This example formulates and solves a simple MIP model:
         try:
             # Create a new model
             gurobi_model = gp.Model("sc_optimization_1")
+            
+            # Create random swing contracts
+            print("Create random swing contracts")
+            self.add_random_swing_contract_offers()
+            # Prepare arrays (all variables of one type in one array, e.g. all prices are in one array)
+            print("Prepare arrays")
+            self.prepare_arrays()
 
             # Create variables for the optimization problem
+            print("Creating Gurobi variables")
             self.create_gurobi_variables(gurobi_model)
-
-            # Set objective function
-            self.set_objective_function(gurobi_model)
-
+            
             # Add constraints
-            self.add_constraints(gurobi_model)
+            # self.add_constraints(gurobi_model)
+            # print("finisehd with adding constraints")
+
+            '''        
+            # Set objective function
+            # self.set_objective_function(gurobi_model)
+            # print("finisehd with seting objective function")
 
             # Optimize model
             gurobi_model.optimize()
@@ -49,13 +70,14 @@ class MarketOptimizer:
             # Print solution of objective function (optimal values of the variables)
             print('Objective value: %g' % gurobi_model.ObjVal)
 
+            '''
         except gp.GurobiError as e:
             print('Error code ' + str(e.errno) + ': ' + str(e))
 
         except AttributeError:
             print('Encountered an attribute error')
-
-
+        
+            
 
 
         return 0
@@ -63,21 +85,51 @@ class MarketOptimizer:
 
     # Create variables for the optimization problem
     def create_gurobi_variables(self, gurobi_model):
-        # Create variables
-        self.x = gurobi_model.addVar(vtype=GRB.BINARY, name="x")
-        self.y = gurobi_model.addVar(vtype=GRB.BINARY, name="y")
-        self.z = gurobi_model.addVar(vtype=GRB.BINARY, name="z")
+        # offer_price
+        self.keys_offer_price_dollar = gurobi_model.addVars(self.number_of_swing_contract_offers, lb=0, vtype=GRB.CONTINUOUS, name="offer_price_dollar")
+        # power_min_mw of a generator, power_max_mw of a generator
+        self.keys_power_min_mw = gurobi_model.addVars(self.number_of_swing_contract_offers, lb=0, vtype=GRB.CONTINUOUS, name="power_min_mw")
+        self.keys_power_max_mw = gurobi_model.addVars(self.number_of_swing_contract_offers, lb=0, vtype=GRB.CONTINUOUS, name="power_max_mw")
+        # max_ramping_up in mw and max_ramping_down in mw of a generator
+        self.keys_ramping_max_mw_up_per_k = gurobi_model.addVars(self.number_of_swing_contract_offers, lb=0, vtype=GRB.CONTINUOUS, name="ramping_max_mw_up_per_k")
+        self.keys_ramping_max_mw_down_per_k = gurobi_model.addVars(self.number_of_swing_contract_offers, lb=0, vtype=GRB.CONTINUOUS, name="ramping_max_mw_down_per_k")
+        # price_per_mw_h_dollar of a generator
+        self.keys_price_per_mw_h_dollar = gurobi_model.addVars(self.number_of_swing_contract_offers, lb=0, vtype=GRB.CONTINUOUS, name="price_per_mw_h_dollar")
+        # is a swing contract cleared
+        self.keys_is_cleared = gurobi_model.addVars(self.number_of_swing_contract_offers, vtype=GRB.BINARY, name="is_cleared")
+        # the (to select) power of a generator in a time step k
+        self.keys_power_mw_in_step_k = gurobi_model.addVars(self.number_of_swing_contract_offers, self.number_of_time_steps_k_in_market, lb=0, vtype=GRB.CONTINUOUS, name="power_mw_in_step_k")
 
+        
     # Set objective function for the optimization problem
     def set_objective_function(self, gurobi_model):
-        # Set objective
-        gurobi_model.setObjective(self.x + self.y + 2 * self.z, GRB.MAXIMIZE)
-
+        print("Set the objective function")
 
     # Add constraints for the optimization problem
     def add_constraints(self, gurobi_model):
-        # Add constraint: x + 2 y + 3 z <= 4
-        gurobi_model.addConstr(self.x + 2 * self.y + 3 * self.z <= 4, "c0")
+        # Set the offer prices in the model to the correct offer prices from the swing contract offers
+        gurobi_model.addConstrs(((self.keys_offer_price_dollar[i] == self.swing_contract_offers[i].offer_price_dollar) for i in range(self.number_of_swing_contract_offers) ), name="c1" )
 
-        # Add constraint: x + y >= 1
-        gurobi_model.addConstr(self.x + self.y >= 1, "c1")
+
+    def add_random_swing_contract_offers(self):
+        # Create three sample swing contracts 
+        # This is a list of SwingContractGenerator objects
+        self.swing_contract_offers = []
+        swing_contract_offer1 = SwingContractGenerator(offer_price_dollar=1, delivery_location="A", powermin_mw=0, powermax_mw=100, ramping_max_mw_up_per_k=10, ramping_max_mw_down_per_k=10, price_per_mw_h_dollar=100)
+        swing_contract_offer2 = SwingContractGenerator(offer_price_dollar=3, delivery_location="A", powermin_mw=0, powermax_mw=100, ramping_max_mw_up_per_k=10, ramping_max_mw_down_per_k=10, price_per_mw_h_dollar=100)
+        swing_contract_offer3 = SwingContractGenerator(offer_price_dollar=3, delivery_location="A", powermin_mw=0, powermax_mw=100, ramping_max_mw_up_per_k=10, ramping_max_mw_down_per_k=10, price_per_mw_h_dollar=100)
+       
+        # add swing contracts to the list of swing contract offers
+        self.swing_contract_offers.append(swing_contract_offer1)
+        self.swing_contract_offers.append(swing_contract_offer2)
+        self.swing_contract_offers.append(swing_contract_offer3)
+
+    def prepare_arrays(self):
+        for sc in self.swing_contract_offers: 
+            self.offer_price_dollar.append(sc.offer_price_dollar)  
+            self.power_min_mw.append(sc.powermin_mw)
+            self.power_max_mw.append(sc.powermax_mw)
+            self.ramping_max_mw_up_per_k.append(sc.ramping_max_mw_up_per_k)
+            self.ramping_max_mw_down_per_k.append(sc.ramping_max_mw_down_per_k)
+            self.price_per_mw_h_dollar.append(sc.price_per_mw_h_dollar)
+            self.number_of_swing_contract_offers += 1
--- classes/swing_contract_generator.py
+++ classes/swing_contract_generator.py
@@ -1,29 +1,70 @@
 class SwingContractGenerator:
-    
+
+
+    # OFFER PRICE
     # the price for clearing this contract
     offer_price_dollar = 0
 
-    # minimum power, a generator can generate
-    powermin_mw = 0
 
-    # maximum power, a generator can generate
+
+
+    # COLLECTION OF POSSIBLE EXERCISE TIMES 
+    # negligible becaus this is a swing contract in firm form (only one exercise time that coincides with the close of the market M(T) )
+
+
+
+
+    # SET OF POSSIBLE POWER PATHS
+    # deliery location of the contract (From A to Z)
+    delivery_location = ""
+
+    # start time of the contract 
+    # neglibible because only one market is taken into account M(T) 
+
+    # minium down / up time 
+    # neglibible because there are no constraints taken into account referring to the minimum up / down time of the generator
+
+    # minimum power, a generator can generate, maximum power, a generator can generate
+    powermin_mw = 0
     powermax_mw = 0
 
     # ramping condition: maximum change in power between two time steps
     ramping_max_mw_up_per_k = 0
     ramping_max_mw_down_per_k = 0
 
+    # duration limits
+    # neglibible because there are no constraints taken into account referring to the duration of the duration limits 
+
+
+
+
+
+    # PERFORMANCE PAYMENT METHOD
     # cost function of the generator: Linear cost function. 
     # The cost function is a function which gives the price for generating a certain amount of power between the minimum and maximum power.
-    # costfunction = 0;
+    # costfunction should be a linear cost function for the power delivered;
+    price_per_mw_h_dollar = 0
 
     # cost function: number of discretization steps
-    costfunction_discretization_steps = 10
+    # costfunction_discretization_steps = 10
+    # neglibible because the cost fucntion is linear and therefore no discretization is needed
 
 
     
     
-    # variables that are normaly not needed for generator but are needed for the optimization
-    
+    # variables that are normaly not needed for generator / swing_contract but are needed for the optimization
     # is this contract cleared from the market?
     is_cleared = False
+
+    # the power in step k of M(T) (24 hours -> 24 steps k)
+    power_mw_in_step_k = []
+
+
+    def __init__(self, offer_price_dollar, delivery_location, powermin_mw, powermax_mw, ramping_max_mw_up_per_k, ramping_max_mw_down_per_k, price_per_mw_h_dollar):
+        self.offer_price_dollar = offer_price_dollar
+        self.delivery_location = delivery_location
+        self.powermin_mw = powermin_mw
+        self.powermax_mw = powermax_mw
+        self.ramping_max_mw_up_per_k = ramping_max_mw_up_per_k
+        self.ramping_max_mw_down_per_k = ramping_max_mw_down_per_k
+        self.price_per_mw_h_dollar = price_per_mw_h_dollar
